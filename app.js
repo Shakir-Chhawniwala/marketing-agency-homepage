463bbd1022702868067880578a8cab07ed718bf5
Shakir-Chhawniwala <64503320+Shakir-Chhawniwala@users.noreply.github.com>
2021-08-27 11:16:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const modal = document.querySelector(".modal"); //selects the modal
+const btnOpenModal = document.querySelector(".show-modal"); //selects the button to show the modal
+const overlay = document.querySelector(".overlay");
+
+const toggleModal = function () {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+btnOpenModal.addEventListener("click", toggleModal);
+
+overlay.addEventListener("click", toggleModal);
+
+var form = document.getElementById("form");
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append("name", document.querySelector('input[name="name"]').value);
+  formData.append(
+    "lastName",
+    document.querySelector('input[name="lname"]').value
+  );
+  formData.append("email", document.querySelector('input[name="email"]').value);
+
+    fetch("https://getform.io/f/abd93388-ee37-4b34-a8a2-9739304d23b4", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+}
